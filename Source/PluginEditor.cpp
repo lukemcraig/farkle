af5564ad3206c857befd033032cb5b9ae500ae9a
Luke M Craig <lukemcduffiecraig@gmail.com>
2018-02-15 20:59:35 -0500

    initial commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+  Farkle effect by Luke M Craig
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+FarkleAudioProcessorEditor::FarkleAudioProcessorEditor (FarkleAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+FarkleAudioProcessorEditor::~FarkleAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void FarkleAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+}
+
+void FarkleAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,34 @@
+/*
+  ==============================================================================
+
+  Farkle effect by Luke M Craig
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+
+//==============================================================================
+/**
+*/
+class FarkleAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    FarkleAudioProcessorEditor (FarkleAudioProcessor&);
+    ~FarkleAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    FarkleAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FarkleAudioProcessorEditor)
+};
